594e975a0af56a0e072f1966662402bff6928809
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-25 11:19:21 +0200

    Add BaseCard UI component

--- src/components/learing-resources/LearningResource.vue
+++ src/components/learing-resources/LearningResource.vue
@@ -1,15 +1,17 @@
 <template>
   <li>
-    <div>
-      <header>
-        <h3>{{ title }}</h3>
-        <button>Delete</button>
-      </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+    <base-card>
+      <div>
+        <header>
+          <h3>{{ title }}</h3>
+          <button>Delete</button>
+        </header>
+      </div>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
